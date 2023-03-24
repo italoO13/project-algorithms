846907ea7d40094c25e5868a2b3827f4113f1fbd
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-03-24 15:19:59 -0300

    test: teste da função encrypt_message req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("msg encriptada", 0) == "adatpircne gsm"
+    assert encrypt_message("hello msg", 3) == "leh_gsm ol"
+    assert encrypt_message("hello msg", 4) == "gsm o_lleh"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("hello msg", "text")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(6486, 1)
