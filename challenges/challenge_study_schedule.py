2737f42077def8dca7bb52ad718a6adde0c2004c
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-03-24 08:31:03 -0300

    refactor: resolvido problema de complexidade do código

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,5 @@
-def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
-    if not isinstance(permanence_period, list) or len(permanence_period) == 0:
-        return None
+def count_periods(permanence_period, target_time):
     contador = 0
-
     for (period_start, period_end) in permanence_period:
         if not isinstance(period_start, int) or not isinstance(
             period_end, int
@@ -13,3 +8,11 @@ def study_schedule(permanence_period, target_time):
         if target_time >= period_start and target_time <= period_end:
             contador += 1
     return contador
+
+
+def study_schedule(permanence_period, target_time):
+    if target_time is None:
+        return None
+    if not isinstance(permanence_period, list) or len(permanence_period) == 0:
+        return None
+    return count_periods(permanence_period, target_time)
