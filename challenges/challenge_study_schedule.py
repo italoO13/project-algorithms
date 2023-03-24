0004fbffe73a299b84ddd6b6cfa705931169ac02
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-03-24 08:24:14 -0300

    feat: implementado função chanllege_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    if not isinstance(permanence_period, list) or len(permanence_period) == 0:
+        return None
+    contador = 0
+
+    for (period_start, period_end) in permanence_period:
+        if not isinstance(period_start, int) or not isinstance(
+            period_end, int
+        ):
+            return None
+        if target_time >= period_start and target_time <= period_end:
+            contador += 1
+    return contador
