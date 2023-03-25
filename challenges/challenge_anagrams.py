123f2e5cd388107e697b76a937cd682b91310ca9
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-03-25 13:07:16 -0300

    fix: alterado algoritmo de ordenação do req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,50 +1,38 @@
-def bubble_sort(word):
-    n = len(word.lower())
-    list_letters = list(word.lower())
-    for j in range(n - 1):
-        for i in range(n - 1):
-            if list_letters[i + 1] < list_letters[i]:
-                list_letters[i + 1], list_letters[i] = (
-                    list_letters[i],
-                    list_letters[i + 1],
-                )
-    return "".join(list_letters)
+def merge_sort(lista, start=0, end=None):
+    if end is None:
+        end = len(lista)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(lista, start, mid)
+        merge_sort(lista, mid, end)
+        merge(lista, start, mid, end)
+    return "".join(lista)
 
 
-# def merge_sort(lista, start=0, end=None):
-#     if end is None:
-#         end = len(lista)
-#     if (end - start) > 1:
-#         mid = (start + end) // 2
-#         merge_sort(lista, start, mid)
-#         merge_sort(lista, mid, end)
-#         merge(lista, start, mid, end)
+def merge(lista, start, mid, end):
+    left = lista[start:mid]
+    right = lista[mid:end]
 
+    left_index, right_index = 0, 0
 
-# def merge(lista, start, mid, end):
-#     left = lista[start:mid]
-#     right = lista[mid:end]
-
-#     left_index, right_index = 0, 0
-
-#     for general_index in range(start, end):
-#         if left_index >= len(left):
-#             lista[general_index] = right[right_index]
-#             right_index = right_index + 1
-#         elif right_index >= len(right):
-#             lista[general_index] = left[left_index]
-#             left_index = left_index + 1
-#         elif left[left_index] < right[right_index]:
-#             lista[general_index] = left[left_index]
-#             left_index = left_index + 1
-#         else:
-#             lista[general_index] = right[right_index]
-#             right_index = right_index + 1
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            lista[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            lista[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            lista[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            lista[general_index] = right[right_index]
+            right_index = right_index + 1
 
 
 def is_anagram(first_string, second_string):
-    first_string_sort = "".join(bubble_sort(first_string))
-    second_string_sort = "".join(bubble_sort(second_string))
+    first_string_sort = "".join(merge_sort(list(first_string.lower())))
+    second_string_sort = "".join(merge_sort(list(second_string.lower())))
 
     if (
         first_string_sort == ""
